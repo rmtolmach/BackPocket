76519a10f4c728f16f1c64aa0dac746190a826a4
Rebecca Tolmach <rmtolmach@gmail.com>
2016-02-23 11:58:37 -0800

    fixed models to have a reference association

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,12 @@
 class Game
   include Mongoid::Document
-  belongs_to :user
-
   field :name, type: String
   field :num_of_players, type: Range
   field :equipment, type: String
   field :noise, type: Mongoid::Boolean
   field :time_range, type: Range
   field :description, type: String
-  field :user_id, type: Integer
+
+  validates_presence_of :name
+  referenced_in :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User
   include Mongoid::Document
-  has_many :games
-  
   field :username, type: String
   field :email, type: String
+  key :_id
+  validates_presence_of :username
+  references_many :games
 end
