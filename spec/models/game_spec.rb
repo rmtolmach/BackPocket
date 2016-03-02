4f9262d482a43caedff08a728b84ece44d53f8ec
Rebecca Tolmach <rmtolmach@gmail.com>
2016-03-02 15:54:10 -0800

    one passing test for game model

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let(:tag)   { Game.create(name: "Tag", equipment: "ball", description: "run, chase, laugh") }
+
+    it "requires a name" do
+      g = Game.new
+      expect(g).to_not be_valid
+      expect(g.errors.keys).to include :name
+    end
+  end
 end
