509f8b16491cd64d0c4d552025887d21a2994f43
Rebecca Tolmach <rmtolmach@gmail.com>
2016-03-10 15:56:40 -0800

    OAuth with facebook working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User
   include Mongoid::Document
+  field :uid, type: String
   field :username, type: String
   field :email, type: String
+  field :oauth_token, type: String
+  field :oauth_expires_at, type: Time
   validates_presence_of :username
   has_and_belongs_to_many :games
+
+  def self.from_omniauth(auth)
+    where(uid: auth.uid).first_or_create do |user|
+      user.uid = auth.uid
+      user.username = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   get 'games' => 'games#index'
   get 'login' => 'games#login'
+  get "/auth/facebook"
+  get "/auth/facebook/callback", to: "sessions#create"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
